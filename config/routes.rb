c53be143b5ff22b5f4e19ff8604dbcf376c63ab1
Ryoji <ryyoshida09@gmail.com>
2018-08-14 16:06:32 +0000

    CRUD tasklsits

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "tasklists#index"
+  resources :tasklists
+  
+  #本来のルーティング内容
+  #get "tasklists/:id", to: "tasklists#show"
+  #post "tasklists", to: "tasklists#create"
+  #put "tasklists/:id", to: "tasklists#update"
+  #delete "tasklists/:id", to:"tasklists#destroy"
+  
+  #index: showの補助ページ
+  #get "tasklists", to: "tasklists#index"
+  
+  #new: 新規作成用のフォームページ
+  #get "tasklists/new", to: "tasklists#new"
+  
+  #edit: 更新用のフォームページ
+  #get "tasklists/:id/edit", to: "tasklists#edit"
 end
